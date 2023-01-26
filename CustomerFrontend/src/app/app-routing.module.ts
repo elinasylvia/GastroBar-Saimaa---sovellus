396b3ac8c2cf5faac38039a28c9a06456c4a83f9
welu563 <ellu__1992@hotmail.com>
2023-01-26 13:11:43 +0200

    changing places in routing

--- CustomerFrontend/src/app/app-routing.module.ts
+++ CustomerFrontend/src/app/app-routing.module.ts
@@ -1,24 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+
+
 import { BillingComponent } from './billing/billing.component';
 import { ItemPageOneComponent } from './item-page-one/item-page-one.component';
 import { ItemPageTwoComponent } from './item-page-two/item-page-two.component';
-import { LoginComponent } from './login/login.component';
-import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { StaffOrdersComponent } from './staff-orders/staff-orders.component';
 import { TestiComponent } from './testi/testi.component';
-import { WelcomeComponent } from './welcome/welcome.component';
+
 
 // reitit
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'welcome', component: WelcomeComponent },
   { path: 'cart', component: ShoppingCartComponent },
+
+
   { path: 'testi', component: TestiComponent },
   { path: 'item/1', component: ItemPageOneComponent },
   { path: 'item/2', component: ItemPageTwoComponent },
   { path: 'staff-orders', component: StaffOrdersComponent },
-  { path: 'billing', component: BillingComponent }
+  { path: 'billing', component: BillingComponent },
 
 ];
 
