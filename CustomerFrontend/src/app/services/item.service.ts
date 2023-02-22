d77a35e9404ddfc97c9e072d8dba25447424b400
welu563 <ellu__1992@hotmail.com>
2023-02-22 11:04:43 +0200

    route changed for item2

--- CustomerFrontend/src/app/services/item.service.ts
+++ CustomerFrontend/src/app/services/item.service.ts
@@ -26,7 +26,7 @@ export class ItemService {
     this.url = "https://localhost:7011/api/items/";
 
     this.url2 = "https://localhost:7011/api/items/1";
-    this.url3 = "https://localhost:7011/api/items/3";
+    this.url3 = "https://localhost:7011/api/items/2";
 
     // jos haettaisiin pöytänumeron perusteella
     this.basketUrl = "https://localhost:7011/api/baskets/table/" + this.logincodeService.getText();
